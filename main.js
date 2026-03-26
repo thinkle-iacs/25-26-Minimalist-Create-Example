1a807e05a942eb932c5dd250c951b56738007c11
Tom Hinkle (IACS) <thinkle@innovationcharter.org>
2026-03-26 15:17:12 +0000

    current state of affairs

--- main.js
+++ main.js
@@ -1,18 +1,44 @@
 import "./style.css";
 import { TextInterface } from "text-interface";
 
-let app = document.querySelector("#app");
-// Create a new "Text Interface"
-let ti = new TextInterface(app, "Example Text Interface");
-ti.promptNumber("What is your favorite number?");
-let favoriteNumber = await ti.readNumber();
-ti.output("What is your name?");
-let name = await ti.readText();
-if (name === "Alice" || name === "Bob") {
-  ti.output(`Hello, ${name}!`);
+const app = document.querySelector("#app");
+const ti = new TextInterface(app);
+
+function distance(p1, p2) {
+  let xsquared = (p1[0] - p2[0]) ** 2;
+  let ysquared = (p1[1] - p2[1]) ** 2;
+  return Math.sqrt(xsquared + ysquared);
 }
-else if (name > "M") {
-  ti.output("Wow your name is late in the alphabet");
-} else {
-  ti.output("What a pretty name!");
+
+function displayGuessFeedback(guesses, target) {
+  ti.output(`Guess\tDistance from Target`);
+  for (let guess of guesses) {
+    let dist = distance(guess, target);
+    ti.output(`(${guess[0]}, ${guess[1]})\t${dist.toFixed(2)} `);
+    if (dist === 0) {
+      ti.output(`(${guess[0]}, ${guess[1]})\tCorrect!`);
+      ti.output('YOU WIN!!!!!!!!')
+      return true;
+    }
+  }
 }
+
+function main() {
+  let targetPoint = [Math.floor(Math.random() * 100), Math.floor(Math.random() * 100)];
+  let guessPoints = [];
+  let gameOver = false;
+  ti.prompt("Welcome to the Guessing Game! Try to guess the target point (x, y) on a 100x100 grid.");
+  while (!gameOver) {
+    let x = ti.promptIntegerInRange("X: ", 0, 100);
+    let y = ti.promptIntegerInRange("Y: ", 0, 100);
+    guessPoints.push([x, y]);
+    gameOver = displayGuessFeedback(guessPoints, targetPoint);
+  }
+  if (ti.promptYesOrNo("Play again? (y/n)")) {
+    main();
+  } else {
+    ti.output("Thanks for playing!");
+  }
+}
+
+main();
