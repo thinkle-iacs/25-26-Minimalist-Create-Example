798977f684b6ea37f0fbf1eac92ec8746a549daa
Tom Hinkle (IACS) <thinkle@innovationcharter.org>
2026-03-26 19:36:24 +0000

    finalize

--- main.js
+++ main.js
@@ -12,33 +12,60 @@ function distance(p1, p2) {
 }
 
 function displayGuessFeedback(guesses, target) {
-  ti.output(`Guess\tDistance from Target`);
+  ti.output(`Guess\tDistance\n-----\t--------`);
   for (let guess of guesses) {
     let dist = distance(guess, target);
     ti.output(`(${guess[0]}, ${guess[1]})\t${dist.toFixed(2)} `);
     if (dist === 0) {
-      ti.output(`(${guess[0]}, ${guess[1]})\tCorrect!!!`);
+      ti.output(`You win!!!`);
       return true;
     }
   }
 }
 
-async function main() {
-  let targetPoint = [Math.floor(Math.random() * 20), Math.floor(Math.random() * 20)];
+async function main(size) {
+  let targetPoint = [Math.floor(Math.random() * size), Math.floor(Math.random() * size)];
   let guessPoints = [];
   let gameOver = false;
-  ti.prompt("Welcome to the Guessing Game! Try to guess the target point (x, y) on a 20x20 grid.");
+  ti.prompt(`Try to guess the target point (x, y) on a ${size}x${size} grid.`);
   while (!gameOver) {
-    let xy = await ti.promptIntegerInRange("X: ", 0, 20);
-    let y = await ti.promptIntegerInRange("Y: ", 0, 20);
+    let x = await ti.promptIntegerInRange("X: ", 0, size);
+    let y = await ti.promptIntegerInRange("Y: ", 0, size);
     guessPoints.push([x, y]);
     gameOver = displayGuessFeedback(guessPoints, targetPoint);
   }
   if (await ti.promptYesOrNo("Play again? (y/n)")) {
-    await main();
+    let newSize = await ti.promptInteger("Enter new grid size: ");
+    await main(newSize);
   } else {
     ti.output("Thanks for playing!");
   }
 }
 
-await main();
+await main(
+  10
+);
+
+
+/*
+In order check whether each point in guessPoints is valid, 
+I would first set a variable isValid to true, with the assumption
+that all points are valid.
+
+Then I would need to loop through each point in guessPoints and check if it is valid by calling the function isValid(value). 
+
+
+If any point is found to be invalid, I would know I had an invalid
+list.
+
+*/
+
+/*
+The parameter to my function is `size`. In my first call, I call
+main(10) which creates a grid of size 10. By using the parameter,
+I am able to make it easy to change the size of the grid. That wAY,
+the same code works for a grid with size 20 or 40, and I could easily
+change the game to have the grid grow each time you play, or, as I actually do, ask the user for a new grid size each time they play again.
+
+
+*/
